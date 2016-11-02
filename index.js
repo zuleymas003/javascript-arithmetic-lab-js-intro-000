def82bb51166fd2207d8faa5edfcb7ee7263c616
zuleymas003 <zuleymas27684@students.laalliance.org>
2016-11-02 15:51:55 +0000

    First saved version of my nitrous code.

---
+++ index.js
@@ -0,0 +1,24 @@
+var add = function(a,b){
+  return a+b;
+};
+var subtract = function (a,b){
+  return a - b;
+};
+var multiply  = function (a,b){
+  return a * b;
+};
+var divide = function (a,b){
+  return a / b;
+};
+var inc = function (n){
+  return n + 1;
+};
+var dec = function (n){
+  return n - 1;
+};
+var makeInt = function (string){
+  return parseInt(string, 10);
+};
+var preserveDecimal = function (string){
+  return parseFloat (string,10);
+};
